f9e624c350ac9563f810ba74276b4333e441fb40
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-02-20 17:02:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+attendees.map do |attendees|
+  batch =[]
+  attendees.join(" ")
+  badger<< batch.each
+ end
+end
